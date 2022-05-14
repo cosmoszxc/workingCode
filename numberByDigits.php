003e68433b0560283346dd3635a5c3e7a29a4131
cosmoszxc <104221390+cosmoszxc@users.noreply.github.com>
2022-05-14 23:07:08 +0200

    Update numberByDigits.php

--- numberByDigits.php
+++ numberByDigits.php
@@ -1,7 +1,7 @@
 <?php
 
 $number = random_int(1000, 9999);
-#$one_thousand = intval($number / 1000); #тысяча
+#$one_thousand = intval($number / 1000); #тысяча приведенная к int
 $one_thousand = (int)($number / 1000); #тысяча приведенная к int
 $hundred = ($number / 100) % 10; #сотня
 $ten = ($number / 10) % 10;#десяток
